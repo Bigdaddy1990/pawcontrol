5e27742209ce1b810520c71d562f65ed9bc55cb9
Bigdaddy1990 <91220458+Bigdaddy1990@users.noreply.github.com>
2025-08-18 06:49:29 +0200

    Delete tests directory

--- tests/test_config_flow_bindings.py
+++
@@ -1,4 +0,0 @@
-def test_config_flow_importable():
-    import custom_components.pawcontrol.config_flow as cf
-
-    assert hasattr(cf, "ConfigFlow") or True
--- tests/test_device_actions.py
+++
@@ -1,5 +0,0 @@
-def test_actions_module_importable():
-    import custom_components.pawcontrol.device_action as device_action
-
-    assert hasattr(device_action, "async_get_actions")
-    assert "toggle_geofence_alerts" in device_action.ACTION_TYPES
