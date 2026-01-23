5addcd4c6b46d1ffaefb16717045bfc0d5b6f8f9
Bigdaddy1990 <91220458+Bigdaddy1990@users.noreply.github.com>
2026-01-23 17:25:10 +0100

    Re-export dog health placeholders

--- custom_components/pawcontrol/config_flow_dogs.py
+++ custom_components/pawcontrol/config_flow_dogs.py
@@ -93,14 +93,22 @@ from custom_components.pawcontrol.types import (
 )
 
 from .flows.gps import _build_dog_gps_placeholders
+from .flows.health import _build_dog_health_placeholders
 from .selector_shim import selector
 
 _LOGGER = logging.getLogger(__name__)
 
 # TODO: Refactor callers to import _build_dog_gps_placeholders from .flows.gps directly.
 build_dog_gps_placeholders = _build_dog_gps_placeholders
+# TODO: Refactor callers to import _build_dog_health_placeholders from .flows.health directly.
+build_dog_health_placeholders = _build_dog_health_placeholders
 
-__all__ = ("_build_dog_gps_placeholders", "build_dog_gps_placeholders")
+__all__ = (
+  "_build_dog_gps_placeholders",
+  "_build_dog_health_placeholders",
+  "build_dog_gps_placeholders",
+  "build_dog_health_placeholders",
+)
 
 _TRANSLATIONS_IMPORT_PATH = "homeassistant.helpers.translation"
 _ASYNC_GET_TRANSLATIONS: Callable[..., Awaitable[dict[str, str]]] | None
